8cf61e42a626dd4c3b3a46544c388d55f49c4dab
zviroth <zviroth@gmail.com>
2018-10-03 16:41:06 -0400

    for lab laptop

---
+++ startup.m
@@ -0,0 +1,5 @@
+addpath(genpath('/Users/lbctest/Documents/MATLAB/rewardStim'));
+addpath(genpath('/Users/lbctest/Documents/MATLAB/mgl'));
+addpath(genpath('/Users/lbctest/Documents/MATLAB/gru'));
+addpath(genpath('/Users/lbctest/Documents/MATLAB/mrTools'));
+addpath(genpath('/Users/lbctest/Documents/MATLAB/matlabPyrTools'));
